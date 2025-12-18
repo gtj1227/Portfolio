1461846b4f7e7ef9ac18451d2000841a88f6639b
gtj1227 <gtjereos@gmail.com>
2025-12-17 23:31:07 -0500

    Customized title-tab

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { ThemeToggle } from "/components/ThemeToggle";
 import { StarBackground } from "/components/StarBackground";
 import { NavBar } from "/components/NavBar";
@@ -8,7 +8,14 @@ import { SkillsSection } from "../../components/SkillsSection";
 import { ProjectsSection } from "../../components/ProjectsSection";
 import { ContactSection } from "../../components/ContactSection";
 import { Footer } from "/components/Footer";
+
+
 export const Home = () => {
+
+  useEffect(() => {
+    document.title = 'George Jereos - Portfolio';
+  }, []) //Empty array dependency to run only once on mount
+  
   return (
     <div className= "min-h-screen bg-background text-foreground overflow-x-hidden">
 
